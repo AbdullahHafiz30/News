12d165db0d3a7c35f6575a86df24f0e83f46b424
ABDULLAH HAFIZ <ABDULLAH_MH30@OUTLOOK.COM>
2025-03-23 12:08:45 +0300

    Initial Commit

---
+++ News/NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsApp.swift
+//  News
+//
+//  Created by Abdullah Hafiz on 23/09/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
